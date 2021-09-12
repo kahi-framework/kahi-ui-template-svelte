2b3d813a1dda391c5bb59e8e62796f979f4383a9
novacbn <31122674+novacbn@users.noreply.github.com>
2021-09-12 00:45:18 -0400

    - Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+	import {Anchor, Container, Heading, Text} from "@kahi-ui/framework";
+
+	export let name;
+</script>
+
+<Container is="main">
+	<Heading>Hello {name}!</Heading>
+	<Text>Visit the <Anchor href="https://svelte.dev/tutorial">Svelte tutorial</Anchor> to learn how to build Svelte apps.</Text>
+</Container>
